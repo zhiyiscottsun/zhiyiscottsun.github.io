85b24ab5b134fd736c0f3017f0194056d71aae6c
zhiyiscottsun <118780832+zhiyiscottsun@users.noreply.github.com>
2025-10-01 02:04:49 +0000

    Deploying to gh-pages from @ zhiyiscottsun/zhiyiscottsun.github.io@78406b3072c9f6735c7943f537056ec94f6dbae7 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "zhiyiscottsun/zhiyiscottsun",
+    "data-repo": "zhiyiscottsun/zhiyiscottsun.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
