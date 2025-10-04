87a20669a2fd657f51a57917dcc0f319f7a40c70
zhiyiscottsun <118780832+zhiyiscottsun@users.noreply.github.com>
2025-10-04 06:09:39 +0000

    Deploying to gh-pages from @ zhiyiscottsun/zhiyiscottsun.github.io@810761d67599c87ec199c2197091f9276e1117bf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -393,15 +393,7 @@ ninja.data = [{
             window.location.href = "/conferences-events/2015/formatting-and-links/";
           
         },
-      },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
+      },{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
           section: "News",},{
