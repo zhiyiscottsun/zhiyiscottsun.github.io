e34a3c8adae71167cbbc99f9536a5c4f85b1b1a3
zhiyiscottsun <118780832+zhiyiscottsun@users.noreply.github.com>
2025-10-04 06:05:54 +0000

    Deploying to gh-pages from @ zhiyiscottsun/zhiyiscottsun.github.io@70e530f3286284174e3cd6bd1659e0fa40ad8988 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -404,7 +404,22 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-7",
+          section: "News",},{id: "projects-project-4",
+          title: 'project 4',
+          description: "another without an image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/4_project/";
+            },},{id: "projects-project-5",
+          title: 'project 5',
+          description: "a project with a background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
+            },},{id: "projects-project-6",
+          title: 'project 6',
+          description: "a project with no image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/6_project/";
+            },},{id: "projects-project-7",
           title: 'project 7',
           description: "with background image",
           section: "Projects",handler: () => {
