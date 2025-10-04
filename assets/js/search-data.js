6282f9807425b923bc0b065a7eb352dc0b0243be
zhiyiscottsun <118780832+zhiyiscottsun@users.noreply.github.com>
2025-10-04 08:10:07 +0000

    Deploying to gh-pages from @ zhiyiscottsun/zhiyiscottsun.github.io@bdffffa48788e724d5c690311b2e15fa4d1e5246 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -394,34 +394,6 @@ ninja.data = [{
           
         },
       },{
-        id: 'social-email',
-        title: 'email',
-        section: 'Socials',
-        handler: () => {
-          window.open("mailto:%7A%68%69%79%69%73%75%6E@%75%6E%63.%65%64%75", "_blank");
-        },
-      },{
-        id: 'social-github',
-        title: 'GitHub',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://github.com/zhiyiscottsun", "_blank");
-        },
-      },{
-        id: 'social-linkedin',
-        title: 'LinkedIn',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.linkedin.com/in/zhiyi-sun-5939381b1", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=f50Hxw0AAAAJ", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
