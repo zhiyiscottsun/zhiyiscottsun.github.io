1354931c36163cede44e187659c8e2c8ed517c2e
zhiyiscottsun <118780832+zhiyiscottsun@users.noreply.github.com>
2025-10-04 06:10:20 +0000

    Deploying to gh-pages from @ zhiyiscottsun/zhiyiscottsun.github.io@2ecbf3935284466ef79c3016c621376d3664a3b4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -393,15 +393,7 @@ ninja.data = [{
             window.location.href = "/conferences-events/2015/formatting-and-links/";
           
         },
-      },{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{
+      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
