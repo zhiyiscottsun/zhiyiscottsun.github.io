890d57f2d8810782c0495801a3e726b4715acbe9
zhiyiscottsun <118780832+zhiyiscottsun@users.noreply.github.com>
2025-10-05 06:59:08 +0000

    Deploying to gh-pages from @ zhiyiscottsun/zhiyiscottsun.github.io@b4f7fc2c6e749ee5a153a110330e2e1068cb8c85 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,7 +30,73 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{
+        },{id: "post-supporting-healthcare-planning-and-resource-management-for-gastritis-and-duodenitis-using-large-language-models",
+        
+          title: "Supporting Healthcare Planning and Resource Management for Gastritis and Duodenitis Using Large Language...",
+        
+        description: "AMIA 2025 Clinical Informatics Conference, Anaheim, CA",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/conferences-events/2025/amia-cic-2025-gastritis-duodenitis-planning/";
+          
+        },
+      },{id: "post-using-large-language-models-for-child-friendly-narratives-in-pediatric-ibd-education",
+        
+          title: "Using Large Language Models for Child-Friendly Narratives in Pediatric IBD Education",
+        
+        description: "Society of Behavioral Medicine (SBM 2025 Annual Meeting), San Francisco, CA",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/conferences-events/2025/sbm-2025-ibd-education/";
+          
+        },
+      },{id: "post-enhancing-pediatric-ibd-management-with-large-language-models-personalized-nutritional-advice-psychological-support-and-educational-tools",
+        
+          title: "Enhancing Pediatric IBD Management with Large Language Models: Personalized Nutritional Advice, Psychological Support,...",
+        
+        description: "IEEE-EMBS International Conference on Biomedical and Health Informatics (BHI&#39;24), Houston, TX",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/conferences-events/2024/embs-bhi-2024-llm-ibd-management/";
+          
+        },
+      },{id: "post-human-guided-iterative-prompt-engineering-for-precision-feedback-message-authoring-using-llms",
+        
+          title: "Human-Guided Iterative Prompt Engineering for Precision Feedback Message Authoring Using LLMs",
+        
+        description: "ADSA 2024 Annual Meeting, Ann Arbor, MI",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/conferences-events/2024/adsa-2024-precision-feedback-prompts/";
+          
+        },
+      },{id: "post-iterative-prompt-engineering-using-llms-for-authoring-precision-feedback-messages",
+        
+          title: "Iterative Prompt Engineering Using LLMs for Authoring Precision Feedback Messages",
+        
+        description: "MCBK Global Meeting 2024 - Lightning Talk (Virtual)",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/conferences-events/2024/mcbk-2024-iterative-prompt-engineering/";
+          
+        },
+      },{id: "post-reviewer-2025-aspph-annual-meeting-for-academic-public-health",
+        
+          title: "Reviewer - 2025 ASPPH Annual Meeting for Academic Public Health",
+        
+        description: "Association of Schools and Programs of Public Health (ASPPH), Oct 2024",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/conferences-events/2024/aspph-2025-reviewer/";
+          
+        },
+      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
