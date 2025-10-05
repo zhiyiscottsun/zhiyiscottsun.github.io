4bc80a772478d6df5ccccd236d379c906b44613b
zhiyiscottsun <118780832+zhiyiscottsun@users.noreply.github.com>
2025-10-05 07:52:04 +0000

    Deploying to gh-pages from @ zhiyiscottsun/zhiyiscottsun.github.io@2cbcf84b0ab6ca25191c92ec928d025a9f02c984 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -78,7 +78,7 @@ ninja.data = [{
         
           title: "Iterative Prompt Engineering Using LLMs for Authoring Precision Feedback Messages",
         
-        description: "MCBK (Mobilizing Computable Biomedical Knowledge) Global Meeting 2024",
+        description: "MCBK Global Meeting 2024, Virtual",
         section: "Posts",
         handler: () => {
           
