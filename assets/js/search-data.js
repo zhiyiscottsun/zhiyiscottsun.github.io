41e81c2ccf866993c1c076a76999598e685990b1
zhiyiscottsun <118780832+zhiyiscottsun@users.noreply.github.com>
2025-10-05 00:08:43 +0000

    Deploying to gh-pages from @ zhiyiscottsun/zhiyiscottsun.github.io@30a4d8822c4e82e8159cc3e8c8b3ef2f819be714 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -107,28 +107,6 @@ ninja.data = [{
             window.location.href = "/conferences-events/2024/pseudocode/";
           
         },
-      },{id: "post-a-post-with-code-diff",
-        
-          title: "a post with code diff",
-        
-        description: "this is how you can display code diffs",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/conferences-events/2024/code-diff/";
-          
-        },
-      },{id: "post-a-post-with-advanced-image-components",
-        
-          title: "a post with advanced image components",
-        
-        description: "this is what advanced image components could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/conferences-events/2024/advanced-images/";
-          
-        },
       },{id: "post-a-post-with-vega-lite",
         
           title: "a post with vega lite",
@@ -138,39 +116,6 @@ ninja.data = [{
         handler: () => {
           
             window.location.href = "/conferences-events/2024/vega-lite/";
-          
-        },
-      },{id: "post-a-post-with-geojson",
-        
-          title: "a post with geojson",
-        
-        description: "this is what included geojson code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/conferences-events/2024/geojson-map/";
-          
-        },
-      },{id: "post-a-post-with-echarts",
-        
-          title: "a post with echarts",
-        
-        description: "this is what included echarts code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/conferences-events/2024/echarts/";
-          
-        },
-      },{id: "post-a-post-with-chart-js",
-        
-          title: "a post with chart.js",
-        
-        description: "this is what included chart.js code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/conferences-events/2024/chartjs/";
           
         },
       },{id: "post-displaying-external-posts-on-your-al-folio-blog",
